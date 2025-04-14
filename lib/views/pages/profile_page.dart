a1efcceb2277f42ceb84d51cfc5324e4bfe8893b
Purnadi <purnadi.winarno@automate-app.com>
2025-04-14 22:16:18 +0700

    input: textfield

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Profile Page"));
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onChanged: (value) {
+              setState(() {});
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Text("User enter: ${nameController.text}")],
+          ),
+        ],
+      ),
+    );
   }
 }
