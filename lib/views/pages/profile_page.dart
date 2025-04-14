16004a635ccfb8ff7b04ecc44776f6569c9ae980
Purnadi <purnadi.winarno@automate-app.com>
2025-04-14 22:31:57 +0700

    input: checkbox

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -10,6 +10,8 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController nameController = TextEditingController();
+  bool isChecked = false;
+  bool isCheckedListTile = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -30,6 +32,37 @@ class _ProfilePageState extends State<ProfilePage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [Text("User enter: ${nameController.text}")],
+          ),
+          Row(
+            children: [
+              Checkbox(
+                shape: CircleBorder(),
+                value: isChecked,
+                onChanged: (value) {
+                  setState(() {
+                    isChecked = value!;
+                  });
+                },
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Text("is Checked: $isChecked")],
+          ),
+          CheckboxListTile(
+            contentPadding: EdgeInsets.all(0.0),
+            title: Text("Checkbox List Tile"),
+            value: isCheckedListTile,
+            onChanged: (value) {
+              setState(() {
+                isCheckedListTile = value!;
+              });
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Text("is Checked List Tile: $isCheckedListTile")],
           ),
         ],
       ),
