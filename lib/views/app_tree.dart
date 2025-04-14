25414c7a2402ba7d344a7b3d95bf218b741614bd
Purnadi <purnadi.winarno@automate-app.com>
2025-04-14 15:56:53 +0700

    defining pages

--- lib/views/app_tree.dart
+++ lib/views/app_tree.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_start/views/pages/home_page.dart';
+import 'package:flutter_start/views/pages/profile_page.dart';
 import 'package:flutter_start/widgets/navbar_widget.dart';
+
+List<Widget> pages = [const HomePage(), const ProfilePage()];
 
 class AppTree extends StatelessWidget {
   const AppTree({super.key});
@@ -8,7 +12,7 @@ class AppTree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("My App")),
-
+      body: pages[0],
       bottomNavigationBar: NavbarWidget(),
     );
   }
