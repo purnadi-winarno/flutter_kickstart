fe2710ad7b09fd7f242ed6a256f1f5c5a8b9ca41
Purnadi <purnadi.winarno@automate-app.com>
2025-04-15 07:52:01 +0700

    input: SingleChildScrollview & Image

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -31,72 +31,75 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: nameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
+              onChanged: (value) {
+                setState(() {});
+              },
             ),
-            onChanged: (value) {
-              setState(() {});
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [Text("User enter: ${nameController.text}")],
-          ),
-          Row(
-            children: [
-              Checkbox(
-                shape: CircleBorder(),
-                value: isChecked,
-                onChanged: (value) {
-                  setState(() {
-                    isChecked = value!;
-                  });
-                },
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [Text("is Checked: $isChecked")],
-          ),
-          CheckboxListTile(
-            contentPadding: EdgeInsets.all(0.0),
-            title: Text("Checkbox List Tile"),
-            value: isCheckedListTile,
-            onChanged: (value) {
-              setState(() {
-                isCheckedListTile = value!;
-              });
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [Text("is Checked List Tile: $isCheckedListTile")],
-          ),
-          Row(
-            children: [
-              Switch.adaptive(value: isSwitched, onChanged: onChangedSwitch),
-            ],
-          ),
-          SwitchListTile(
-            title: Text("Switch List Tile"),
-            value: isSwitched,
-            onChanged: onChangedSwitch,
-          ),
-          Slider(
-            max: 100,
-            divisions: 10,
-            activeColor: Colors.lightGreenAccent,
-            value: sliderValue,
-            onChanged: onChangedSlider,
-          ),
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Text("User enter: ${nameController.text}")],
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  shape: CircleBorder(),
+                  value: isChecked,
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Text("is Checked: $isChecked")],
+            ),
+            CheckboxListTile(
+              contentPadding: EdgeInsets.all(0.0),
+              title: Text("Checkbox List Tile"),
+              value: isCheckedListTile,
+              onChanged: (value) {
+                setState(() {
+                  isCheckedListTile = value!;
+                });
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Text("is Checked List Tile: $isCheckedListTile")],
+            ),
+            Row(
+              children: [
+                Switch.adaptive(value: isSwitched, onChanged: onChangedSwitch),
+              ],
+            ),
+            SwitchListTile(
+              title: Text("Switch List Tile"),
+              value: isSwitched,
+              onChanged: onChangedSwitch,
+            ),
+            Slider(
+              max: 100,
+              divisions: 10,
+              activeColor: Colors.lightGreenAccent,
+              value: sliderValue,
+              onChanged: onChangedSlider,
+            ),
+            Image.asset("assets/images/animal.jpg"),
+          ],
+        ),
       ),
     );
   }
