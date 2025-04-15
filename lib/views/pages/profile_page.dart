5d6c612131fcc86de50207ee732338d9c58a53d1
Purnadi <purnadi.winarno@automate-app.com>
2025-04-15 09:00:33 +0700

    clean up the code

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+// Models
+class ProfileState {
+  final TextEditingController nameController;
+  final bool isChecked;
+  final bool isCheckedListTile;
+  final bool isSwitched;
+  final double sliderValue;
+  final String? dropdownValue;
+
+  ProfileState({
+    required this.nameController,
+    this.isChecked = false,
+    this.isCheckedListTile = false,
+    this.isSwitched = false,
+    this.sliderValue = 0.0,
+    this.dropdownValue,
+  });
+
+  ProfileState copyWith({
+    bool? isChecked,
+    bool? isCheckedListTile,
+    bool? isSwitched,
+    double? sliderValue,
+    String? dropdownValue,
+  }) {
+    return ProfileState(
+      nameController: nameController,
+      isChecked: isChecked ?? this.isChecked,
+      isCheckedListTile: isCheckedListTile ?? this.isCheckedListTile,
+      isSwitched: isSwitched ?? this.isSwitched,
+      sliderValue: sliderValue ?? this.sliderValue,
+      dropdownValue: dropdownValue ?? this.dropdownValue,
+    );
+  }
+}
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,35 +44,144 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  TextEditingController nameController = TextEditingController();
-  bool isChecked = false;
-  bool isCheckedListTile = false;
-  bool isSwitched = false;
-  double sliderValue = 0.0;
-  String? dropdownValue;
+  // Remove 'final' keyword to make _state mutable
+  ProfileState _state = ProfileState(nameController: TextEditingController());
 
-  void onChangedSwitch(bool value) {
-    setState(() {
-      isSwitched = value;
-    });
+  // Remove initState since we're initializing _state directly
+
+  @override
+  void dispose() {
+    _state.nameController.dispose();
+    super.dispose();
   }
 
-  void onChangedSlider(double value) {
-    setState(() {
-      sliderValue = value;
-    });
-  }
-
-  void onButtonPressed(String buttonType) {
-    print("$buttonType button pressed");
-  }
-
-  // Change the type declaration from List<Widget> to List<DropdownMenuItem<String>>
-  List<DropdownMenuItem<String>> dropdownItems = [
+  static const List<DropdownMenuItem<String>> _dropdownItems = [
     DropdownMenuItem(value: "Option 1", child: Text("Option 1")),
     DropdownMenuItem(value: "Option 2", child: Text("Option 2")),
     DropdownMenuItem(value: "Option 3", child: Text("Option 3")),
   ];
+
+  void _onChangedSwitch(bool value) => setState(() {
+    _state = _state.copyWith(isSwitched: value);
+  });
+
+  void _onChangedSlider(double value) => setState(() {
+    _state = _state.copyWith(sliderValue: value);
+  });
+
+  void _onButtonPressed(String buttonType) {
+    debugPrint("$buttonType button pressed");
+  }
+
+  void _onCheckboxChanged(bool? value) => setState(() {
+    _state = _state.copyWith(isChecked: value ?? false);
+  });
+
+  void _onCheckboxListTileChanged(bool? value) => setState(() {
+    _state = _state.copyWith(isCheckedListTile: value ?? false);
+  });
+
+  void _onDropdownChanged(String? value) => setState(() {
+    _state = _state.copyWith(dropdownValue: value);
+  });
+
+  Widget _buildTextField() => TextField(
+    controller: _state.nameController,
+    decoration: InputDecoration(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+    ),
+    onChanged: (_) => setState(() {}),
+  );
+
+  Widget _buildCheckbox() => Checkbox(
+    shape: const CircleBorder(),
+    value: _state.isChecked,
+    onChanged: _onCheckboxChanged,
+  );
+
+  Widget _buildCheckboxListTile() => CheckboxListTile(
+    contentPadding: EdgeInsets.zero,
+    title: const Text("Checkbox List Tile"),
+    value: _state.isCheckedListTile,
+    onChanged: _onCheckboxListTileChanged,
+  );
+
+  Widget _buildSwitch() =>
+      Switch.adaptive(value: _state.isSwitched, onChanged: _onChangedSwitch);
+
+  Widget _buildSwitchListTile() => SwitchListTile(
+    title: const Text("Switch List Tile"),
+    value: _state.isSwitched,
+    onChanged: _onChangedSwitch,
+  );
+
+  Widget _buildSlider() => Slider(
+    max: 100,
+    divisions: 10,
+    activeColor: Colors.lightGreenAccent,
+    value: _state.sliderValue,
+    onChanged: _onChangedSlider,
+  );
+
+  Widget _buildInkWellContainer() => InkWell(
+    splashColor: Colors.red,
+    onTap: () => debugPrint("box pressed"),
+    borderRadius: BorderRadius.circular(10),
+    child: Container(
+      width: double.infinity,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white12,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+  );
+
+  Widget _buildButtonsWrap() => Wrap(
+    children: [
+      ElevatedButton(
+        onPressed: () => _onButtonPressed("Elevated"),
+        child: const Text("Elevated Button"),
+      ),
+      FilledButton(
+        style: FilledButton.styleFrom(
+          backgroundColor: Colors.lightGreenAccent,
+          foregroundColor: Colors.black,
+        ),
+        onPressed: () => _onButtonPressed("Filled"),
+        child: const Text("Filled Button"),
+      ),
+      OutlinedButton(
+        onPressed: () => _onButtonPressed("Outline"),
+        child: const Text("Outline Button"),
+      ),
+      TextButton(
+        onPressed: () => _onButtonPressed("Text"),
+        child: const Text("Text Button"),
+      ),
+      CloseButton(onPressed: () => _onButtonPressed("Close")),
+      BackButton(onPressed: () => _onButtonPressed("Back")),
+    ],
+  );
+
+  Widget _buildDropdown() => Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      border: Border.all(color: Colors.grey),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    child: DropdownButton<String>(
+      value: _state.dropdownValue,
+      items: _dropdownItems,
+      onChanged: _onDropdownChanged,
+      isExpanded: true,
+      icon: const Icon(Icons.arrow_drop_down_circle, color: Colors.deepPurple),
+      underline: Container(),
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      borderRadius: BorderRadius.circular(8),
+      hint: const Text('Select an option'),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -46,137 +190,29 @@ class _ProfilePageState extends State<ProfilePage> {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              onChanged: (value) {
-                setState(() {});
-              },
-            ),
+            _buildTextField(),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [Text("User enter: ${nameController.text}")],
+              children: [Text("User enter: ${_state.nameController.text}")],
             ),
-            Row(
-              children: [
-                Checkbox(
-                  shape: CircleBorder(),
-                  value: isChecked,
-                  onChanged: (value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  },
-                ),
-              ],
-            ),
+            Row(children: [_buildCheckbox()]),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [Text("is Checked: $isChecked")],
+              children: [Text("is Checked: ${_state.isChecked}")],
             ),
-            CheckboxListTile(
-              contentPadding: EdgeInsets.all(0.0),
-              title: Text("Checkbox List Tile"),
-              value: isCheckedListTile,
-              onChanged: (value) {
-                setState(() {
-                  isCheckedListTile = value!;
-                });
-              },
-            ),
+            _buildCheckboxListTile(),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [Text("is Checked List Tile: $isCheckedListTile")],
-            ),
-            Row(
               children: [
-                Switch.adaptive(value: isSwitched, onChanged: onChangedSwitch),
+                Text("is Checked List Tile: ${_state.isCheckedListTile}"),
               ],
             ),
-            SwitchListTile(
-              title: Text("Switch List Tile"),
-              value: isSwitched,
-              onChanged: onChangedSwitch,
-            ),
-            Slider(
-              max: 100,
-              divisions: 10,
-              activeColor: Colors.lightGreenAccent,
-              value: sliderValue,
-              onChanged: onChangedSlider,
-            ),
-            InkWell(
-              splashColor: Colors.red,
-              onTap: () {
-                print("box pressed");
-              },
-              borderRadius: BorderRadius.circular(
-                10,
-              ), //don't forget this too -> as BoxDecoration size
-              child: Container(
-                width: double.infinity,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Colors.white12,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            Wrap(
-              children: [
-                ElevatedButton(
-                  onPressed: () => onButtonPressed("Elevated"),
-                  child: Text("Elevated Button"),
-                ),
-                FilledButton(
-                  style: FilledButton.styleFrom(
-                    backgroundColor: Colors.lightGreenAccent,
-                    foregroundColor: Colors.black,
-                  ),
-                  onPressed: () => onButtonPressed("Filled"),
-                  child: Text("Filled Button"),
-                ),
-                OutlinedButton(
-                  onPressed: () => onButtonPressed("Outline"),
-                  child: Text("Outline Button"),
-                ),
-                TextButton(
-                  onPressed: () => onButtonPressed("Text"),
-                  child: Text("Text Button"),
-                ),
-                CloseButton(onPressed: () => onButtonPressed("Close")),
-                BackButton(onPressed: () => onButtonPressed("Back")),
-              ],
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: DropdownButton<String>(
-                value: dropdownValue,
-                items: dropdownItems,
-                onChanged: (String? value) {
-                  setState(() {
-                    dropdownValue = value;
-                  });
-                },
-                isExpanded: true,
-                icon: const Icon(
-                  Icons.arrow_drop_down_circle,
-                  color: Colors.deepPurple,
-                ),
-                underline: Container(), // removes the default underline
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                borderRadius: BorderRadius.circular(8),
-                hint: const Text('Select an option'),
-              ),
-            ),
+            Row(children: [_buildSwitch()]),
+            _buildSwitchListTile(),
+            _buildSlider(),
+            _buildInkWellContainer(),
+            _buildButtonsWrap(),
+            _buildDropdown(),
           ],
         ),
       ),
